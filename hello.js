fb7ee078222d8963d1373d9a062be4d251d47a02
SushilNagdev <156531818+SushilNagdev@users.noreply.github.com>
2024-03-17 08:48:27 +0500

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+var phones=['samsung', 'iphone', 1234];
+console.log(phones)
+phones.shift()
+console.log(phones)
+phones.unshift('lemon')
+console.log(phones)
+
+phones.pop()
+console.log(phones)
+phones.push('orange')
+console.log(phones)
